29ec1d9f8e17f2792765531c2301338d4bb4efad
Jeffrey Luo <jl834@duke.edu>
2020-02-01 15:27:49 -0500

    Created Ball class

---
+++ src/breakout/Ball.java
@@ -0,0 +1,27 @@
+package breakout;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+/**
+ * Ball that the player wants to keep bouncing.
+ *
+ * @author Jeffrey Luo
+ */
+public class Ball extends Circle {
+
+    public static final double BALL_RADIUS = 0.5;
+    public static final int NORMAL_BALL_SPEED = 50;
+    public static final int SLOW_BALL_SPEED = 20;
+
+    /**
+     * Creates a ball at the given x and y coordinates of the center of the circle.
+     * @param x - x coordinate
+     * @param y - y coordinate
+     */
+    public Ball(int x, int y) {
+        super(x, y, BALL_RADIUS);
+        this.setFill(Color.RED);
+    }
+
+}
