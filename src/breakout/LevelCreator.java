2570c63dd4b2413befae257a852182e4b0c5010c
Roy Xiong <rx28@duke.edu>
2020-02-02 15:15:39 -0500

    Now have multiple bricks whose setup is based on a file

---
+++ src/breakout/LevelCreator.java
@@ -0,0 +1,44 @@
+package breakout;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class LevelCreator {
+
+    public static final double BRICKS_RATIO_TO_SCREEN = 0.6;
+
+    public static List<Brick> setupBricksForLevel(String path, int gameWidth, int gameHeight) {
+        int numRows = getNumberRows(path);
+        int numColumns = getNumberColumns(path);
+        List<Brick> bricks = new ArrayList<>();
+
+        Scanner scan = getScannerForFile(path);
+        for(int i = 0; i < numRows; i++) {
+            String line = scan.nextLine();
+            for(int j = 0; j < numColumns; j++) {
+                if(line.substring(j, j + 1).equals("1"))
+                    bricks.add(new Brick((gameWidth / numColumns) * j, (gameHeight * BRICKS_RATIO_TO_SCREEN) * i / numRows,
+                            gameWidth / numColumns, gameHeight * BRICKS_RATIO_TO_SCREEN / numRows));
+            }
+        }
+        return bricks;
+    }
+
+    private static int getNumberRows(String path) {
+        Scanner scan = getScannerForFile(path);
+        int counter = 0;
+        while(scan.hasNextLine()) {
+            scan.nextLine();
+            counter++;
+        }
+        return counter;
+    }
+    private static int getNumberColumns(String path) {
+        Scanner scan = getScannerForFile(path);
+        return scan.next().length();
+    }
+    private static Scanner getScannerForFile(String path) {
+        return new Scanner(LevelCreator.class.getClassLoader().getResourceAsStream(path));
+    }
+}
