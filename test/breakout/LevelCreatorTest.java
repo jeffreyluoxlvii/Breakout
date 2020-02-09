61862c09236c6ab7312db99e5e636c549eed2400
Roy Xiong <rx28@duke.edu>
2020-02-09 15:39:37 -0500

    created tests for levelcreator

---
+++ test/breakout/LevelCreatorTest.java
@@ -0,0 +1,33 @@
+package breakout;
+
+
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import util.DukeApplicationTest;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class LevelCreatorTest {
+    @Test
+    public void testBrickSetup() {
+        List<Brick> bricks = LevelCreator.setupBricksForLevel("testOne", 80, 80);
+        assertEquals(10, bricks.get(0).getWidth());
+        assertEquals(10, bricks.get(1).getX());
+        assertEquals(80 / 2.0 * LevelCreator.BRICKS_RATIO_TO_SCREEN ,bricks.get(1).getY());
+    }
+
+    @Test
+    public void testBrickSetup2() {
+        List<Brick> bricks = LevelCreator.setupBricksForLevel("testTwo", 40, 100);
+        assertEquals(10, bricks.get(1).getWidth());
+        assertEquals(100 * LevelCreator.BRICKS_RATIO_TO_SCREEN / 5, bricks.get(2).getY());
+        assertEquals(20, bricks.get(2).getX());
+    }
+}
