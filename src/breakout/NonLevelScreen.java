9460f8ae6297cf736bbdc4ca28836ee13589a654
Roy Xiong <rx28@duke.edu>
2020-02-16 21:13:15 -0500

    Refactored nonlevel screen so that no longer an abstract class

--- src/breakout/NonLevelScreen.java
+++ src/breakout/NonLevelScreen.java
@@ -1,14 +1,19 @@
 package breakout;
 
-import javafx.animation.Animation;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 
-import java.util.ArrayList;
-import java.util.List;
-
+/**
+ * The NonLevelScreen is an super-class for all screens that are not an
+ * actual part of the game play. All classes that extend this class will
+ * have the same prompt at the bottom to go to the next screen. Additionally,
+ * they will all have the same location of text within the screen, creating
+ * a uniform feel to the game.
+ *
+ * @author roy
+ */
 public abstract class NonLevelScreen {
 
     public static final String GO_TO_NEXT_SCREEN_TEXT = "Press the [spacebar] to continue";
@@ -21,16 +26,45 @@ public abstract class NonLevelScreen {
     public static final String ADD = "+";
     public static final String EQUALS = "=";
 
-    public abstract Scene getScene();
+    protected Scene myScene;
 
-    protected Text setTextLocation(Text text) {
+    /**
+     * Construct a new NonLevelScreen. Initializes the Scene object with a prompt at the bottom to continue.
+     */
+    public NonLevelScreen() {
+        Group root = new Group();
+        root.getChildren().add(getNextScreenText());
+        myScene = new Scene(root, BreakoutGame.GAME_WIDTH, BreakoutGame.GAME_HEIGHT, BreakoutGame.BACKGROUND);
+    }
+
+    /**
+     * Adds the text to the screen.
+     */
+    public void addTextToScreen(Text text) {
+        ((Group)myScene.getRoot()).getChildren().add(text);
+    }
+
+    /**
+     * Return the Scene associated with this screen.
+     */
+    public Scene getScene() {
+        return myScene;
+    }
+
+    /**
+     * Returns the given text centered in the screen.
+     */
+    public Text setTextLocation(Text text) {
         text.setTextAlignment(TextAlignment.CENTER);
         text.setX(BreakoutGame.GAME_WIDTH / 2 - text.getLayoutBounds().getWidth() / 2);
         text.setY(BreakoutGame.GAME_HEIGHT * INSTRUCTIONS_STARTING_Y);
         return text;
     }
 
-    protected Text getNextScreenText() {
+    /**
+     * This same prompt is added to each screen.
+     */
+    private Text getNextScreenText() {
         Text nextScreenText = new Text();
         nextScreenText.setText(NonLevelScreen.GO_TO_NEXT_SCREEN_TEXT);
         nextScreenText.setX(BreakoutGame.GAME_WIDTH / 2 - nextScreenText.getLayoutBounds().getWidth() / 2);
